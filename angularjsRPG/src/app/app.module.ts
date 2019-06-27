fce0daf812d6a1b1de8094bef0f31167ddfd5993
Derrick Lau <derrick.evltfmr@gmail.com>
2019-06-27 10:45:36 +0800

    Set up the Router and add the components in the project

--- angularjsRPG/src/app/app.module.ts
+++ angularjsRPG/src/app/app.module.ts
@@ -1,16 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms'; // allow us to use the NgModel two way binding for input values in the application
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app.component';
 
+// Import the components
+import { StartComponent } from './components/start/start.component';
+import { CharacterCreationComponent } from './components/character-creation/character-creation.component';
+import { StoryComponent } from './components/story/story.component';
+import { InventoryComponent } from './components/inventory/inventory.component';
+import { FightComponent } from './components/fight/fight.component';
+
+// Set up the routes
+const routes: Routes = [  // create an array of Routes objects
+
+  { path: "", component: StartComponent},
+  { path: "story", component: StoryComponent},
+  { path: "character-creation", component: CharacterCreationComponent},
+  { path: "fight", component: FightComponent},
+  { path: "**", redirectTo: ""}
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // Declare the components here so that we can use them later in our code
+    // This is to tell Angular the components exist
+    StartComponent,
+    CharacterCreationComponent,
+    StoryComponent,
+    InventoryComponent,
+    FightComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes),// This creates our router with all of the routes that we've created
+                                 // so it will know what to use for the paths in the application
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angularjsRPG/src/app/components/story/story.component.ts
+++ angularjsRPG/src/app/components/story/story.component.ts
@@ -4,6 +4,6 @@ import { Component } from '@angular/core';
 
 })
 
-export class StartComponent {
+export class StoryComponent {
 
 }
