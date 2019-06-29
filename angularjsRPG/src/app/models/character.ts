502b1bd63e8481599a495fd78777ebb460a62c54
Derrick Lau <derrick.evltfmr@gmail.com>
2019-06-29 18:01:17 +0800

    Updated enum CharacterSkills, FightOptions, var ExperienceToLevel

--- angularjsRPG/src/app/models/character.ts
+++ angularjsRPG/src/app/models/character.ts
@@ -22,3 +22,33 @@ export class Weapon {
 	minDamage: number;
 	maxDamage: number;
 }
+
+
+export enum CharacterSkills {
+	attack = "attack",
+	sneak = "sneak",
+	persuade = "persuade",
+	intelligence = "intelligence"
+}
+
+
+export enum FightOptions {
+	attack = "Attack",
+	specialAttack = "Special Attack",
+	none = "None"								// This may not be helpful in the fight, but it will be helpful in our code
+												// we can check to see whether we have selected an action
+}
+
+export const ExperienceToLevel = {				// How much experience we need to level up our character
+	1: 1000,
+	2: 2000,
+	3: 3000,
+	4: 4000,
+	5: 5000,
+	6: 6000,
+	7: 7000,
+	8: 8000,
+	9: 9000
+};												// variable usually comes with ;
+
+
