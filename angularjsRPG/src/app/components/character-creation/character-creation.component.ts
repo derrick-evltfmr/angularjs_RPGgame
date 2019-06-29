c3b49a6b440ae031b3d64cac7d170ce7c0ff870a
Derrick Lau <derrick.evltfmr@gmail.com>
2019-06-29 11:26:37 +0800

    Updated character-creation.component.ts

--- angularjsRPG/src/app/components/character-creation/character-creation.component.ts
+++ angularjsRPG/src/app/components/character-creation/character-creation.component.ts
@@ -7,5 +7,15 @@ import { Component } from '@angular/core';
 })
 
 export class CharacterCreationComponent{
+  character = {                         // set up some properties for character
+    race: '--Choose--',                 // set the default to some string
+    class: '--Choose--',
+    gender: undefined,                  // it's ok to set undefined because they are button
+    name: undefined                      // not displaying anything
+  }
+
+  characterComplete:boolean = false     // set a characterComplete flag
+                                        // typescript boolean
+
 
 }
