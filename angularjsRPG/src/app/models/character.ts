db6f6887ce4435456bf8ad4b5549f42aa8cd02f0
Derrick Lau <derrick.evltfmr@gmail.com>
2019-06-29 19:02:24 +0800

    Updated attack(), sneak(), persuade() and dealDamage() in BaseCharacter class

--- angularjsRPG/src/app/models/character.ts
+++ angularjsRPG/src/app/models/character.ts
@@ -78,8 +78,29 @@ export class BaseCharacter {
 		this.currentHealth = health;
 		this.skills = skills;
 		this.isIncapacitated = false;
-
-
 	}
 
+	attack() {
+		return Math.floor(Math.random() * 20) + 1 + this.skills.attack;
+																		// Floor will round a number down to the nearest integer
+																		// random will generate a number between 0 to 1 but never 1, so 0~0.99999999
+																		// 0~19 +1, so 1~20
+	}
+
+	sneak() {
+		return Math.floor(Math.random() * 20) + 1 + this.skills.sneak;
+	}
+
+	persuade() {
+		return Math.floor(Math.random() * 20) + 1 + this.skills.persuade;
+	}
+
+	dealDamage() {
+		return Math.floor(Math.random() * (this.equippedWeapon.maxDamage - this.equippedWeapon.minDamage + 1)) + this.equippedWeapon.minDamage;
+
+		// generate a number from 0 to (weapon.maxdamage - weapon.mindamage) + weapon.mindamage,
+		// so it can reflect the weapon min/max damage difference, but still keep the damage stable
+		// e.g. even if the random number is 0, it will still at least casue minDamage + 0
+
+	}
 }
