7ac27ebf0d929f447e0c28ee57204e3322fbe728
Derrick Lau <derrick.evltfmr@gmail.com>
2019-07-03 15:44:20 +0800

    Fixed character options related to Camps

--- angularjsRPG/src/app/components/character-creation/character-creation.component.ts
+++ angularjsRPG/src/app/components/character-creation/character-creation.component.ts
@@ -9,7 +9,7 @@ import { CharacterOptions } from '../../models/character-options';
 
 export class CharacterCreationComponent{
 	character = {                           // set up some properties for character
-		race: '--Choose--',                 // set the default to some string
+		camp: '--Choose--',                 // set the default to some string
 		class: '--Choose--',
 		gender: undefined,                  // it's ok to set undefined because they are button
 		name: undefined                     // not displaying anything
@@ -18,13 +18,13 @@ export class CharacterCreationComponent{
 	characterComplete: boolean = false;     // set a characterComplete flag
 											// typescript boolean
 
-	races = CharacterOptions.races;
+	camps = CharacterOptions.camps;
 	classes = CharacterOptions.classes;
 	genders = CharacterOptions.genders;
 
 
-	changeRace(race: string) {
-		this.character.race = race;
+	changecamp(camp: string) {
+		this.character.camp = camp;
 		this.checkCompleted();
 	}
 
@@ -43,10 +43,10 @@ export class CharacterCreationComponent{
 	}
 
 	checkCompleted(){
-		this.characterComplete = this.character.race !== "--Choose--"	// if these conditions are all True(1), then 1, because of logical and
+		this.characterComplete = this.character.camp !== "--Choose--"	// if these conditions are all True(1), then 1, because of logical and
 			&& this.character.class !== "--Choose--"
 			&& this.character.gender !== undefined
-			&& this.character.name										// this equals to true, not undefined(false value)
+			&& this.character.name;										// this equals to true, not undefined(false value)
 	}
 
 	createCharacter() {
