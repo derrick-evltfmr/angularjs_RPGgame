68e53fbbf605f7c06778e1a53190d06d04427775
Derrick Lau <derrick.evltfmr@gmail.com>
2019-07-05 23:57:54 +0800

    Fixed Chapter1.ts

--- angularjsRPG/src/app/chapters/Chapter1.ts
+++ angularjsRPG/src/app/chapters/Chapter1.ts
@@ -8,7 +8,7 @@ export const Chapter1: Chapter = {
 		`You enter the woods, chasing after the goblin who stole your father's sword.`,
 		`You lose sight of them in the thick woos and begin to creep forward, relying on your ears`,
 		`to warn you of danger and hopfully to locate the theiving golbin.`,
-		`Shenanigans ensue and an encounter begins. Now the question is: How do you want to handle it?`;
+		`Shenanigans ensue and an encounter begins. Now the question is: How do you want to handle it?`
 	],
 	options: [
 		CharacterAction.attack,
@@ -16,7 +16,7 @@ export const Chapter1: Chapter = {
 		CharacterAction.persuade
 	],
 	enemyParty: [
-		new Monster("Goblin", 5, {attack: 2, sneak: 0, persuade: 0}, {attack: 10, sneak: 10, persuade: 10}, 1, 3, "../../assets/goblin.png");
+		new Monster("Goblin", 5, {attack: 2, sneak: 0, persuade: 0}, {attack: 10, sneak: 10, persuade: 10}, 1, 3, "../../assets/goblin.png")
 	],
 	sneakPersuadeFail: CharacterAction.attack,
 	ifFail: FailureOptions.nextChapter,
@@ -28,6 +28,9 @@ export const Chapter1: Chapter = {
 	rewards: {
 		experience: 500,
 		equipment: [new Weapon("Basic Dagger", 1, 6)],
-		newHero: new Warrior("Benjamin", GenderOptions.male, CampOptions.strengthCountry, 1, 10, {attack: 2, sneak})
-	}
+		newHero: new Warrior("Benjamin", GenderOptions.male, CampOptions.strengthCountry, 1, 10,
+		{attack: 2, sneak:1, persuade: 1, intelligence: 1}, new Weapon("Dagger", 1, 4), new Armor("Cloths",0))
+	},
+	nextChapter: null
+
 };
