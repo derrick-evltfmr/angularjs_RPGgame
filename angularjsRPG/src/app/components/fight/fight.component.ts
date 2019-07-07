d0ecd0179edea8e7e759f464e0c95dc5ff4c1520
Derrick Lau <derrick.evltfmr@gmail.com>
2019-07-07 17:41:45 +0800

    Initialized the variables in Fight Component class

--- angularjsRPG/src/app/components/fight/fight.component.ts
+++ angularjsRPG/src/app/components/fight/fight.component.ts
@@ -16,5 +16,29 @@ enum Teams {
 })
 
 export class FightComponent{
+	consturctor(private gameControllerService: GameControllerService,
+		private router: Router) {}
 
+	HeroTurn: boolean = true;
+	actionDelay: number = this.gameControllerService.actionDelay;
+	turnsBetweenSpecial: number = 2;
+	characterIndex: number = 0;
+	freezeActions: boolean = false;
+
+	heroParty: Hero[] = this.gameControllerService.heroParty;
+	heroesIncapacitated: number = 0;
+	enemyParty: Monster[] = this.gameControllerService.enemyParty;
+	enemiesIncapacitated: number = 0;
+
+	currentCharacter: BaseCharacter = this.heroParty[this.characterIndex];
+	_fightOptions: typeof FightOptions = FightOptions;
+	_teams: typeof Teams = Teams;
+	selectedAction: FightOptions = FightOptions.none;
+	availableTargets: Teams = Teams.none;
+	selectedTargets: BaseCharacter[] = [];
+
+	displayMessage: string = `${this.currentCharacter.name}'s turn.`;
+	successMessages: string[] = [];
+	showNextChapterButton: boolean = false;
+	showGameOverButton: boolean = false;
 }
