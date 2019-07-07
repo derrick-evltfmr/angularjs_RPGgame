f466f3b1a292daa1450dac5f06ea42059c0ad3e2
Derrick Lau <derrick.evltfmr@gmail.com>
2019-07-07 17:41:15 +0800

    Set up the imports for fight component

--- angularjsRPG/src/app/components/fight/fight.component.ts
+++ angularjsRPG/src/app/components/fight/fight.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import { GameControllerService } from '../../services/game-controller.service';
+import { Hero, Monster, BaseCharacter, FightOptions, Warrior, Ranger, Rogue, Priest } from '../../models/characters';
+import { Router } from '@angular/router';
+
+enum Teams {
+	heroes,
+	enemies,
+	none
+}
 
 @Component({
   selector: "fight-component",
