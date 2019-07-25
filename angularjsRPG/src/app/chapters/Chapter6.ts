eb2ca448312ebfb04ff04b451d257e887483fefb
Derrick Lau <derrick.evltfmr@gmail.com>
2019-07-26 00:27:24 +0800

    Updated Chapter6 story

--- angularjsRPG/src/app/chapters/Chapter6.ts
+++ angularjsRPG/src/app/chapters/Chapter6.ts
@@ -4,12 +4,18 @@ import { GenderOptions, CampOptions, ClassOptions } from "../models/character-op
 
 export const Chapter6: Chapter = {
 	story: [
-		`You defeated the treasure monsters, but it seems that the evil wizard has notice your existence`,
-		`So the ruin is starting to collapse, you and your teammates try to escape from the ruin`,
+		`During the fight with the Dark Dragon, two guys appeared and gave you a hand`,
+		`With the help of Benjamin and John, you successfully defeated the Dark Dragon`,
+		`You leave the ruin in time, after you all get out, the ruin collapses `,
 		`'`,
-		`You are almost at the entrance of the ruin, but suddenly a black hole appears at the entrance of the ruin`,
-		`All of you are shocked, because... a huge dark dragon was summoned...`,
-		`it seems that it won't let you to escape from the ruin...`,
+		`You say 'Thanks' to them, they said they are just two random bravers that passing by`,
+		`But they are very interested to join the fight against the evil wizard, so they are now your new teammates`,
+		`'`,
+		`Your party goes through the magic river and comes to a valley, you can feel there are some very strong magic power in the deep inside`,
+		`Everything around becomes darker and darker. And finally you reach a castle.`,
+		`From the dark energy, you can tell the evil wizard must be inside the castle, but your party is not the first that arrives here`,
+		`Inside there are two people's shadows fighting with the monster, but there is a great quantity of the enemies`,
+		`You want to help, and at this time, three monsters are rushing to your side.`,
 		`How do you want to handle it?`
 	],
 
@@ -20,11 +26,11 @@ export const Chapter6: Chapter = {
 	],
 
 	enemyParty: [
-		new Monster("Skull Monster", 60, {attack: 30, sneak: 40, persuade: 40}, {attack: 30, sneak: 40, persuade: 40}, 12, 18,
+		new Monster("Skull Monster", 60, {attack: 30, sneak: 70, persuade: 70}, {attack: 30, sneak: 70, persuade: 70}, 12, 18,
 			"../../assets/skullmonster.png"),
-		new Monster("Dark Pumpkin", 100, {attack: 30, sneak: 40, persuade: 40}, {attack: 30, sneak: 40, persuade: 40}, 14, 25,
+		new Monster("Dark Pumpkin", 100, {attack: 30, sneak: 70, persuade: 70}, {attack: 30, sneak: 70, persuade: 70}, 14, 25,
 			"../../assets/darkpumpkin.png"),
-		new Monster("Bat Monster", 60, {attack: 30, sneak: 40, persuade: 40}, {attack: 30, sneak: 40, persuade: 40}, 12, 18,
+		new Monster("Bat Monster", 60, {attack: 30, sneak: 70, persuade: 70}, {attack: 30, sneak: 70, persuade: 70}, 12, 18,
 			"../../assets/batmonster.png")
 							// (name, health, skills, barriers, minDamage, maxDamage, spriteUrl)
 	],
@@ -40,8 +46,8 @@ export const Chapter6: Chapter = {
 	],
 
 	rewards: {
-		experience: 15000,
-		equipment: [new Armor("Dragon Slayer Suit", 20)],
+		experience: 18000,
+		equipment: [new Weapon("Evil breaker", 18, 30)],
 		newHero: [new Warrior("Benjamin", GenderOptions.male, CampOptions.dexterityCountry, 9, 95, {attack: 70, sneak: 40,
 			persuade: 25, intelligence: 10}, new Weapon("Fire Sword", 10, 15), new Armor("Knight Armor", 15)),
 				new Priest("John", GenderOptions.male, CampOptions.wiseCountry, 9, 80, {attack: 30, sneak: 20,
